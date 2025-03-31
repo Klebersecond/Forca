7c3dc22f2753314cfe677923ed9f61f3d3b47f8d
Kleber Neves <klebernssii@gmail.com>
2025-03-31 08:28:11 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿string palavra, letra;
+Console.WriteLine("Digite uma palavra ");
+palavra = Console.ReadLine()!;
+
+Console.WriteLine("Digite uma letra ");
+letra = Console.ReadLine()!;
+Console.WriteLine($"a letra {letra} existe na palavra {palavra.Contains (letra)}");
